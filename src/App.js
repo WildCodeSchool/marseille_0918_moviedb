978e82e364453ec20f650ec076a1a402e6bd59a2
Reb Sen <archivesreb@gmail.com>
2018-10-19 09:59:56 +0200

    carte film automone pas connectee à la barre de recherche dans le fichier component/cardo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 //This file host all the components of our application
 import React, { Component } from 'react';
-import { BrowserRouter, Route, NavLink, Switch} from 'react-router-dom';
 import './App.css';
 import Home from './Home';
 import MovieCard from './components/movieCard/MovieCard';
@@ -10,34 +9,65 @@ class App extends Component {
 
   render() {
     return (
+    
       
-      <div>
-      <BrowserRouter>
-        <ul> 
-          <li>
-            <NavLink  
-                exact to="/"
-               ><button>Click Here !!</button></NavLink>
-          </li>
-          
-          
-            
-          <li>
-              <NavLink
-                 to="/moviecard" 
-                >MovieCard</NavLink>
-            </li>
-        </ul>
-     
-        <Switch>     
-          <Route exact path="/" component={Home}/> 
-          <Route path="/moviecard" component={MovieCard}/> 
-        </Switch>
-     
-      </BrowserRouter>
-    </div>
+    <h1>page app reb...</h1>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
     );
   }
 }
 
 export default App;
+
+
+
+//import { BrowserRouter, Route, NavLink, Switch} from 'react-router-dom';
+
+// <div>
+//       <BrowserRouter>
+//         <ul> 
+//           <li>
+//             <NavLink  
+//                 exact to="/"
+//                ><button>Click Here !!</button></NavLink>
+//           </li>
+          
+          
+            
+//           <li>
+//               <NavLink
+//                  to="/moviecard" 
+//                 >MovieCard</NavLink>
+//             </li>
+//         </ul>
+     
+//         <Switch>     
+//           <Route exact path="/" component={Home}/> 
+//           <Route path="/moviecard" component={MovieCard}/> 
+//         </Switch>
+     
+//       </BrowserRouter>
+//     </div>
---
+++ src/components/MovieCard-cardo/Cardos.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react'
+import Cardo from './Cardo'
+
+export default class Cardos extends Component {
+constructor(props) {
+    super(props);
+    this.state = {
+        movies: [],
+       
+    }
+}
+
+componentDidMount = () => { 
+    
+    fetch("https://api.themoviedb.org/3/movie/upcoming?api_key=5db1cb3add526f8a1516b0e4b55c9404&language=fr&page=1")
+    .then(response => response.json()) 
+    .then(response => this.setState({movies:response.results}))
+
+} 
+
+
+
+
+
+
+render() {
+    
+    if(this.state.movies.length > 0){
+        console.log("hello", this.state.movies)
+        return (
+        <div>
+            {this.state.movies.map((item,index) => {
+              if (index < 1 ) {
+                return <Cardo
+                       key={index}
+                       title={item.title}
+                       date={item.release_date}
+                       resume={item.overview}
+                       vote={item.vote_average}
+                       voteco={item.vote_count}
+                       backdrop={item.backdrop_path}
+                        />
+                }
+            })
+        }
+        
+      
+    </div>
+    )}
+    return (
+        <div>
+          <h1>No users founds </h1>
+        </div>
+    )
+    }
+  
+}
+
+
+//fetch("https://api.themoviedb.org/3/discover/movie?api_key=5db1cb3add526f8a1516b0e4b55c9404&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1")
