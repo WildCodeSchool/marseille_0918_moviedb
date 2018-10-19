b63b56fc6432597ea07a2cc1ef84c55e0b0aa6d9
Lisa Ferrier <lisaferrrier@gmail.com>
2018-10-19 14:24:40 +0200

    fix files location

--- src/App.js
+++ src/App.js
@@ -2,10 +2,9 @@
 import React, { Component } from 'react';
 import {Route, Switch} from 'react-router-dom';
 import './App.css';
-import MovieCard from './components/movieCard/MovieCard';
 import Home from './components/shared/home/Homes';
 import SearchBar from './components/searchBar/SearchBar';
-import singleMovie from './components/MovieCard/singleMovie';
+import singleMovie from './components/movieCard/singleMovie';
 
 
 class App extends Component {
