ab77b398e0b654b96a4f4bb0e8245eb0a52149a8
Reb Sen <archivesreb@gmail.com>
2018-10-19 09:44:37 +0200

    avant mise sur skeleton

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
 //This file host all the components of our application
 import React, { Component } from 'react';
+import { BrowserRouter, Route, NavLink, Switch} from 'react-router-dom';
 import './App.css';
-import Home from './components/Home';
+import Home from './Home';
+import MovieCard from './components/movieCard/MovieCard';
+
 
 class App extends Component {
 
   render() {
     return (
-
-      <div className="App">
-        <Home />
-      </div>   
-
+      
+      <div>
+      <BrowserRouter>
+        <ul> 
+          <li>
+            <NavLink  
+                exact to="/"
+               ><button>Click Here !!</button></NavLink>
+          </li>
+          
+          
+            
+          <li>
+              <NavLink
+                 to="/moviecard" 
+                >MovieCard</NavLink>
+            </li>
+        </ul>
+     
+        <Switch>     
+          <Route exact path="/" component={Home}/> 
+          <Route path="/moviecard" component={MovieCard}/> 
+        </Switch>
+     
+      </BrowserRouter>
+    </div>
     );
   }
 }
--- src/components/movieCard/MovieCard.js
+++ src/components/movieCard/MovieCard.js
@@ -1,4 +1,37 @@
-import React from "react";
+import React, { Component } from 'react'
+import { Card, CardText, CardBody, CardLink, CardTitle, CardSubtitle } from 'reactstrap';
 
-export default MovieCard;
+
+export default class MovieCard extends Component {
+
+    render() {
+        console.log(this.props.date.split("-").reverse().join("-"))
+    
+        // const {item} = this.props;
+        return (
+          <div>
+          <Card >
+          <CardBody>
+            <p className="sizeletter">Hello</p>
+            <CardSubtitle>Date de sortie : never</CardSubtitle>
+          </CardBody>
+              
+          <CardBody>
+             <CardText>Résumé</CardText>
+             <CardLink href="#">Average vote : va savoir </CardLink>
+             <CardLink href="#">Vote count :</CardLink>
+          </CardBody>
+        </Card>
+            
+    
+          </div>
+        )
+     }
+  
+ 
+    
+}
+
+
+
 
